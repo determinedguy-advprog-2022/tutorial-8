f43c67fae9454cf897a5ae9de02cb2f2213ca4e6
Muhammad Athallah <mhd.athallah@gmail.com>
2022-05-12 00:57:12 +0700

    Finish implementing BookingService

--- booking-service/src/main/java/id/ac/ui/cs/advprog/bookingservice/service/BookingServiceImpl.java
+++ booking-service/src/main/java/id/ac/ui/cs/advprog/bookingservice/service/BookingServiceImpl.java
@@ -4,15 +4,22 @@ import id.ac.ui.cs.advprog.bookingservice.model.Booking;
 import id.ac.ui.cs.advprog.bookingservice.model.dto.CreateBookingDTO;
 import id.ac.ui.cs.advprog.bookingservice.repository.BookingRepository;
 import id.ac.ui.cs.advprog.bookingservice.vo.Asdos;
+import id.ac.ui.cs.advprog.bookingservice.vo.BookingApproval;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BookingServiceImpl implements BookingService {
     @Autowired
     private BookingRepository bookingRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public List<Booking> getAllBooking() {
@@ -22,6 +29,23 @@ public class BookingServiceImpl implements BookingService {
     @Override
     public Booking createBooking(CreateBookingDTO bookingRequest) {
         // ToDo
+        BookingApproval bookingResponse =
+                restTemplate.postForObject("http://ASDOS-SERVICE/asdos/book", bookingRequest, BookingApproval.class);
+
+        // Print as log to make use of the message
+        System.out.println(bookingResponse.getMessage());
+
+        if (bookingResponse.getStatus().equals("Success")) {
+            Booking newBooking = new Booking();
+
+            newBooking.setBookerName(bookingRequest.getName());
+            newBooking.setBookerClass(bookingRequest.getBookerClass());
+            newBooking.setAsdosId(bookingRequest.getAsdosCode());
+            newBooking.setBookTime(bookingRequest.getRequestedTime());
+
+            bookingRepository.save(newBooking);
+            return newBooking;
+        }
         return null;
     }
 
@@ -34,7 +58,8 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public List<Asdos> getAllAsdos() {
-        // ToDo
-        return null;
+        ResponseEntity<Asdos[]> asdosResponse = restTemplate.getForEntity("http://ASDOS-SERVICE/asdos/", Asdos[].class);
+        List<Asdos> asdosList = Arrays.asList(asdosResponse.getBody());
+        return asdosList;
     }
 }
